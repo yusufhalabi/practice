ade694b2d932a6ca2b0929a5a0f1af9cb2bd960b
Yusuf <yusuf@halabi.us>
2020-05-26 02:08:34 -0700

    updated solution

--- wayTooLongWords.py
+++ wayTooLongWords.py
@@ -1,6 +1,7 @@
 for i in range(input()):
     s = raw_input()
-    if s.len()<10: 
-        print s 
-    else: 
-        print "%s %d %s" % (s[0],s.len()-2,s[-1]) 
+    if len(s)<11:
+        print s
+    else:
+        print s[0]+str(len(s)-2)+s[-1]
+    
